65ace5f7450a051bc3de1d00ac947d9bb9c35dc2
XverLxrd <tur-2002@mail.ru>
2025-05-15 20:13:49 +0700

    start deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
+    output: "export",
+    basePath: '/rk_technologies',
+    images: {
       remotePatterns: [
           {hostname: 'cdn2.thecatapi.com'}
       ],
+        unoptimized: true,
   }
 };
 
